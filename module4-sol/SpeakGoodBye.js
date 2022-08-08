355e15b8f47c2cfa1bd3619b44fda6ea0e557234
Hrushikesh Patra <hrushikesh.patra24@gmail.com>
2022-08-08 23:46:23 +0530

    added module 4 sol

---
+++ module4-sol/SpeakGoodBye.js
@@ -0,0 +1,18 @@
+//speakGoodbye.js
+
+
+
+(function (window) {
+
+var speakWord = "Good Bye";
+
+var byeSpeaker={};
+
+byeSpeaker.speak= function (name) {
+  console.log(speakWord + " " + name);
+}
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
+
